ee2029a3fc8c9a809fdfbfcbea6894f002768e86
GShopov9 <gshopov9@gmail.com>
2018-03-28 16:38:45 -0700

    Add files from CL1

---
+++ CL1ex6.py
@@ -0,0 +1,25 @@
+
+#!/usr/bin/env python
+import yaml
+import json
+from pprint import pprint
+"""
+6. Write a Python program that creates a list. One of the elements of the list should be a dictionary with at least two keys. Write this list out to a file using both YAML and JSON formats. The YAML file should be in the expanded form.
+"""
+
+
+my_list = range(6)
+my_list.append("something")
+my_list.append("$55")
+my_list.append({"key1": "atr1", "key2" : "atr2"})
+my_list[-1]["g_key"] = "g_atr"
+
+
+with open("my_first.yaml", "w") as f:
+	yaml.dump(my_list, f, default_flow_style=False)
+
+with open("my_second.json", "w") as f:
+	json.dump(my_list, f)
+
+pprint(my_list)
+
